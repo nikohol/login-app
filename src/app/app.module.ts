1e21f8d7da2a25de39bef511f4831ad5c50e935e
nikohol <nikohol@metropolia.fi>
2017-01-30 13:46:36 +0200

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,53 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { FrontComponent } from './front/front.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { LogoutComponent } from './logout/logout.component';
+
+const routeConfig = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/login'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+ /* {
+    path: 'logout',
+    component: LogoutComponent
+  },*/
+  {
+    path: 'front',
+    component: FrontComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontComponent,
+    TopBarComponent,
+    LoginComponent,
+    RegisterComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routeConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
